d3efb2fffc3e0d2a1c134686c4d11e5899075ce0
Filip Bártek <filip.bartek@hotmail.com>
2014-06-11 12:26:21 +0200

    saveRoi, intCoord, tile_size_*: Separate in dedicated script "save_roi.py"

--- tiles.py
+++ tiles.py
@@ -284,59 +284,9 @@ def GetData(latx,lonx,zoomx):
 
     return coord
 
-def intCoord(lat, lon, zoom=19):
-    import ModestMaps
-    floatCoord = GetData(lat, lon, zoom)
-    result = ModestMaps.Core.Coordinate(int(floatCoord.row), int(floatCoord.column), int(floatCoord.zoom))
-    return result
-
-# These values are used to offset the tiles in the result composed OBJ file
-tile_size_x = 256.0
-tile_size_y = 256.0
-
-def saveRoi(latBegin, latEnd, lonBegin, lonEnd, zoom=19):
-    """
-    Save the region of interest specified by the parameters to an OBJ file
-    accompanied with a MTL file and one or more texture files.
-    """
-
-    import ModestMaps
-    from TileCombiner import TileCombiner
-
-    assert latEnd >= latBegin
-    assert lonEnd >= lonBegin
-
-    coordBegin = intCoord(latBegin, lonBegin, zoom)
-    coordEnd = intCoord(latEnd, lonEnd, zoom)
-
-    columnBegin = coordBegin.column
-    columnEnd = coordEnd.column
-    assert columnEnd >= columnBegin
-    # Transformation swaps row polarity
-    rowEnd = coordBegin.row
-    rowBegin = coordEnd.row
-    assert rowEnd >= rowBegin
-
-    columnNum = columnEnd - columnBegin + 1
-    rowNum = rowEnd - rowBegin + 1
-
-    logging.debug('Tiles in ROI: %d x %d' % (columnNum, rowNum))
-
-    tc = TileCombiner('out', 'out')
-
-    # Get vertices, faces and textures for each tile
-    tiles = []
-    for columnId in range(columnNum):
-        column = columnBegin + columnId
-        for rowId in range(rowNum):
-            row = rowBegin + rowId
-            coord = ModestMaps.Core.Coordinate(row, column, zoom)
-            textures = get_tile_data(coord)
-            offset_x = columnId * tile_size_x
-            offset_y = (rowNum - rowId - 1) * tile_size_y
-            tc.addTile(textures, offset_x, offset_y)
-
 if __name__ == '__main__':
+    from save_roi import saveRoi
+
     prague_lat = 50.0893
     prague_lon = 14.3994
 
